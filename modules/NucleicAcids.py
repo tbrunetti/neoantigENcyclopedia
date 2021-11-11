0de1da6351dc9ed1c443379ef2b06281484f1e94
tbrunetti <brunettt@colorado.edu>
2021-11-11 15:12:42 -0700

    translate method for Rna object

--- modules/NucleicAcids.py
+++ modules/NucleicAcids.py
@@ -1,4 +1,9 @@
 from typing import *
+import sys
+import os
+sys.path.append(os.getcwd())
+from Codon import *
+
 
 class Rna:
     
@@ -6,29 +11,37 @@ class Rna:
     
     RNA_NUCLEOTIDES = ('A', 'U', 'G', 'C')
     
-    def __init__(self, seq : str, rna_base_options = RNA_NUCLEOTIDES) -> None:
-        check_nt = set([seq[pos].upper() for pos in range(0, len(seq))])
+    def __init__(self, sequence : str, rna_base_options = RNA_NUCLEOTIDES) -> None:
+        check_nt = set([sequence[pos].upper() for pos in range(0, len(sequence))])
         if check_nt <= set(rna_base_options):
             print('Confirmed sequence contains exclusive RNA basepairs')
         else:
             raise ValueError('%s is not a valid nucleotide for a RNA object.' % check_nt)
         
-        self.seq = seq.upper()
+        self.sequence = sequence.upper()
         
-    
+        def translate(self, codon_library : Dict[str, Codon]) -> str:
+            protein_seq = []
+            for codon in range(0, len(self.sequence), 3):
+                print(codon)
+                protein_seq.append(codon_library[self.sequence[codon:codon+3]].translate_symbol())
+            
+            return ''.join(protein_seq)
+        
+            
 class Dna:
     dna_pairs = {'A':'T', 'T':'A', 'G':'C', 'C':'G'}
     
     DNA_NUCLEOTIDES = ('A', 'T', 'G', 'C')
     
-    def __init__(self, seq : str, dna_base_options = DNA_NUCLEOTIDES) -> None:
-        check_nt = set([seq[pos].upper() for pos in range(0, len(seq))])
+    def __init__(self, sequence : str, dna_base_options = DNA_NUCLEOTIDES) -> None:
+        check_nt = set([sequence[pos].upper() for pos in range(0, len(sequence))])
         if check_nt <= set(dna_base_options):
             print('Confirmed sequence contains exclusive DNA basepairs')
         else:
             raise ValueError('%s is not a valid nucleotide for a DNA object.' % check_nt)
         
-        self.seq = seq.upper()
+        self.sequence = sequence.upper()
     
     
     def complement(self) -> str:
@@ -37,7 +50,9 @@ class Dna:
         description:
         return:
         '''
-        complement_list = [dna_pairs[base] for base in list(self.seq)]
+        dna_pairs = {'A':'T', 'T':'A', 'G':'C', 'C':'G'}
+
+        complement_list = [dna_pairs[base] for base in list(self.sequence)]
         return ''.join(complement_list)
     
     def reverse(self) -> str:
@@ -46,7 +61,7 @@ class Dna:
         description:
         return:
         '''
-        reverse_list = [base for base in list(self.seq)[::-1]]
+        reverse_list = [base for base in list(self.sequence)[::-1]]
         return ''.join(reverse_list)
     
     def reverse_complement(self) -> str:
@@ -55,7 +70,9 @@ class Dna:
         description:
         return:
         '''
-        reverse_list = [base for base in list(self.seq)[::-1]]
+        dna_pairs = {'A':'T', 'T':'A', 'G':'C', 'C':'G'}
+
+        reverse_list = [base for base in list(self.sequence)[::-1]]
         rc_list = [dna_pairs[base] for base in reverse_list]
         return ''.join(rc_list)
     
@@ -65,7 +82,7 @@ class Dna:
         description:
         return:
         '''
-        return Rna(self.seq.replace('T', 'U'))
+        return Rna(self.sequence.replace('T', 'U'))
 
 
     
